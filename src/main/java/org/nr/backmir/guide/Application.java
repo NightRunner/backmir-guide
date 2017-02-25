813f2bfabb015c4d214d15a67f139ea6e86b91af
nightrunner <690732060@qq.com>
2017-02-25 21:04:21 +0800

    menu.js和util.js加入版本

--- src/main/java/org/nr/backmir/guide/Application.java
+++ src/main/java/org/nr/backmir/guide/Application.java
@@ -9,21 +9,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 @Controller
 @SpringBootApplication
 public class Application {
+    private static final Date SERVER_START_DATE = new Date();
+
     public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
     }
 
+    private void setCommonVarToModel(Model model) {
+        model.addAttribute("serverStartDate", SERVER_START_DATE);
+    }
+
     @RequestMapping(value = {"/", "", "index"})
     public String index(HttpServletRequest request, HttpServletResponse response, Model model) {
+        setCommonVarToModel(model);
         return "index";
     }
 
     @RequestMapping(value = "/{pageName}")
-    public String page(@PathVariable(value = "pageName") String pageName) {
+    public String page(@PathVariable(value = "pageName") String pageName, Model model) {
+        setCommonVarToModel(model);
         return pageName;
     }
 }
